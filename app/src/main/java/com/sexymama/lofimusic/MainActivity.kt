92228c48a3fd06a228186015da0cd3a2ab5198fe
Varenyam Bhardwaj <varenyambhardwaj123@gmail.com>
2022-10-08 00:41:10 +0530

    fixed imports and package location removed connection tester

--- app/src/main/java/com/sexymama/lofimusic/MainActivity.kt
+++ app/src/main/java/com/sexymama/lofimusic/MainActivity.kt
@@ -3,13 +3,7 @@ package com.sexymama.lofimusic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.sexymama.lofimusic.ui.player
 import com.sexymama.lofimusic.ui.theme.LoFiMusicTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/sexymama/lofimusic/ui/Lofi.kt
+++ app/src/main/java/com/sexymama/lofimusic/ui/Lofi.kt
@@ -1,13 +1,8 @@
-package com.sexymama.lofimusic
+package com.sexymama.lofimusic.ui
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
 import androidx.compose.foundation.Canvas
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -20,35 +15,9 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.sexymama.lofimusic.R
 
-fun isNetworkAvailable(context: Context?): Boolean {
-    if (context == null) return false
-    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-        if (capabilities != null) {
-            when {
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
-                    return true
-                }
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
-                    return true
-                }
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
-                    return true
-                }
-            }
-        }
-    } else {
-        val activeNetworkInfo = connectivityManager.activeNetworkInfo
-        if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
-            return true
-        }
-    }
-    return false
-}
-
-val connected : Boolean = isNetworkAvailable()
+const val connected: Boolean = true
 
 @Composable
 fun player() {
@@ -83,10 +52,8 @@ fun player() {
                     Text(text = "Disconnected", color = Color.White, fontSize = 25.sp)
                 }
             }
-            
+
             Spacer(modifier = Modifier.size(30.dp))
-
-
         }
     }
 }
